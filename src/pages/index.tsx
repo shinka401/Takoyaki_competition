2ae3ea6e007ba85b305b738cc62519318b1fa6eb
shinka401 <shinka401unauthorized@gmail.com>
2023-03-15 22:45:11 +0900

    ♻️ use const function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
+import { NextPage } from "next";
+const Home: NextPage = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+};
+export default Home;
